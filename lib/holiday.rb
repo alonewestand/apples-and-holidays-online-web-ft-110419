a31396b47cf4d9969e7a938c83f596681930b571
Ken Goad <goadke@g.cofc.edu>
2019-11-09 06:03:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -16,8 +16,6 @@ holiday_supplies = {
   }
 }
 
-holiday_supplies[:winter].to_str + holiday_supplies
-
 def second_supply_for_fourth_of_july(holiday_hash)
   holiday_hash[:summer][:fourth_of_july][1]
 end
