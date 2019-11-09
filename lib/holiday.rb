3b943008574d6144fafecc97a25f8589ad0b3a0f
Ken Goad <goadke@g.cofc.edu>
2019-11-09 05:54:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,7 @@ def all_supplies_in_holidays(hash)
     holiday_supplies.each do |season, holiday|
       puts "#{season}".capitalize + ":"
     holiday.each do |holiday, supplies|
-      puts " " + "#{holiday}"
+      puts " " + "#{holiday}".capitalize
     holiday.each do puts " " + "#{supplies}".each 
 end
 
